de60a3c96654a00b8c06b9feecc9e5c262e86779
alexandria branner <ahartpro87@gmail.com>
2020-02-16 02:58:13 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,7 +1,8 @@
 // ♥ irb
   def introduction (Josh)
-   puts "Hi, my name is #{Josh}.
+   puts "Hi, my name is #{Josh}."
 end
 
-  def introduction_with_language (name,language)
-    puts 
+  def introduction_with_language (Dan,Ember.js.)
+    puts "Hi, my name is #{Dan} and I am learning to program in #{Ember.js.}."
+  end
